a591056eead0ac31a75f062c3181e93c6d068d7a
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-01-09 22:11:39 -0500

    Remove header and add global root css varaibles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import styled, { injectGlobal } from 'styled-components';
 
 import './index.css'
 
@@ -14,8 +15,6 @@ const Header = () => (
   >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
         padding: '1.45rem 1.0875rem',
       }}
     >
@@ -43,11 +42,8 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
       }}
@@ -56,6 +52,13 @@ const TemplateWrapper = ({ children }) => (
     </div>
   </div>
 )
+
+injectGlobal`
+  :root {
+    --backgroundColor: #cccccc;
+    --textColor: #141414;
+  }
+`;
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
