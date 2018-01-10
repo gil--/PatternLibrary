8b8ebab5ceb07e4bbe79bc5387e227b3595e24fd
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-01-09 22:11:22 -0500

    Add example structure of data config

---
+++ src/data/settings.js
@@ -0,0 +1,85 @@
+export const data = {
+    title: "Something Digital Pattern Library",
+    version: "1.2.1",
+    sections: [
+        {
+            title: "Colors",
+            type: "color",
+            groups: [
+                {
+                    group: "Base Theme Colors",
+                    options: [
+                        {
+                            title: "Black",
+                            properties: {
+                                color: "#000"
+                            }
+                        },
+                        {
+                            title: "Basenji",
+                            subtitle: "Promo Banner",
+                            properties: {
+                                color: "#fff0c3"
+                            }
+                        },
+                        {
+                            title: "Labrador",
+                            subtitle: "Mobile Menu Tabs",
+                            properties: {
+                                color: "#ffda7d"
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            title: "Typography",
+            type: "typography",
+            groups: [
+                {
+                    group: "Headings",
+                    options: [
+                        {
+                            title: "H1",
+                            html: "<h1>This is an H1</h1>",
+                            properties: {
+                                'font-size': "22px",
+                                'font-size--desktop': "24px"
+                            }
+                        },
+                        {
+                            title: "H1",
+                            html: "<h1>This is a PDP H1</h1>",
+                            properties: {
+                                'font-size': "22px",
+                                'font-size--desktop': "24px"
+                            }
+                        },
+                        {
+                            title: "H2",
+                            html: "<h2>This is an H2</h2>",
+                            properties: {
+                                'font-size': "16px",
+                                'font-size--desktop': "18px"
+                            }
+                        }
+                    ]
+                },
+                {
+                    group: "Body",
+                    options: [
+                        {
+                            title: "Body",
+                            html: "<p>Whether it's singing them to sleep and gently kissing their forhead to show signs of love, or cuddling after a bump on the head, being a parent never feels easy, but it always feels like the best thing you've ever done.</p><p><a href=\"#\">Burt's Bees Baby</a> can help. We are committed to delivering you the best <a href=\"#\">organic product</a> on the market, at prices you can afford.</p>",
+                            properties: {
+                                'font-size': "22px",
+                                'font-size--desktop': "24px"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
