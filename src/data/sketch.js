92046c40990e2254fa0d72bc4f55ae73a197daa6
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-01-11 18:25:55 -0500

    Add example sketch export

---
+++ src/data/sketch.js
@@ -0,0 +1,18 @@
+const data = {
+    title: "Something Digital Pattern Library",
+    version: "1.2.1",
+    url: "https://somethingdigital.com",
+    pages: [
+        {
+            title: "Colors",
+            type: "colors",
+            symbols: [
+                {
+                    title: "Black",
+                    properties: [
+                    ]
+                }
+            ]
+        }
+    ]
+}
