017e18203610286f02217fa183b6b224bc133ce0
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-01-17 18:06:32 -0500

    Adjust section title styling

--- src/components/section-title.js
+++ src/components/section-title.js
@@ -19,12 +19,25 @@ const Title = styled.div`
     justify-content: center;
     min-height: 100px;
     width: 100%;
+    margin-top: 20px;
     text-align: center;
     text-transform: uppercase;
-    background-color: var(--backgroundColor);
+    /* background-color: var(--backgroundColor); */
     z-index: 5;
 
     h2 {
+        position: relative;
         margin: 0;
+
+        &:after {
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            transform: translate(-50%, 10px);
+            width: 75%;
+            height: 3px;
+            background-color: var(--backgroundColor);
+            content: '';
+        }
     }
 `;
