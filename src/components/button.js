1d5d88d887436c39ea06cb831eb46883aa184fe4
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-01-24 23:22:34 -0500

    Make button fullwidth and remove border-radius

--- src/components/button.js
+++ src/components/button.js
@@ -14,10 +14,12 @@ const ButtonStyle = styled.button`
     display: flex;
     align-items: center;
     justify-content: center;
+    /*width: ${props => props.fullWidth ? '100%' : 'auto'};*/
+    width: 100%;
     padding: 10px 25px;
     background-color: white;
     border: 0;
-    border-radius: 25px;
+    /* border-radius: 25px; */
     box-shadow: 0 0 20px #0000001a;
     transition: transform .2s ease-out;
     text-transform: uppercase;
